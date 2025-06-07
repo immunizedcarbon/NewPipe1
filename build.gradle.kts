1fd71e1268a2a982c85181cb71ea7d6cea18403b
immunizedcarbon <98087886+immunizedcarbon@users.noreply.github.com>
2025-06-07 02:28:02 +0200

    Fix build configuration and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version "1.24.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
 }
 
 buildscript {
@@ -10,7 +10,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("com.android.tools.build:gradle:8.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
         // NOTE: Do not place your application dependencies here; they belong
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 pluginManagement {
-    repositories { gradlePluginPortal(); google(); mavenCentral() }
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
     plugins {
         id("com.android.application") version "8.6.0" apply false
         id("com.android.library")     version "8.6.0" apply false
